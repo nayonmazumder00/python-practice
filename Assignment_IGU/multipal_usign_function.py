9f082b92c5c060fa126d0329519ac9dbbd710a03
Nayon Mazumder <nayonmazumder00@gmail.com>
2022-09-18 15:39:16 +0530

    2nd program

---
+++ Assignment_IGU/multipal_usign_function.py
@@ -0,0 +1,14 @@
+""" Name: NAYON MAZUMDER
+    REG : 210011015059".
+    Dept. Of CSE, IGU, INDIA
+"""
+
+def mult(a,b,c):
+    d=a*b*c
+    print("The multi of Variables : ", d)
+
+a=int(input("Please Enter the value of A : "))
+b=int(input("Please Enter the value of B : "))
+c=int(input("Please Enter the value of C : "))
+
+mult(a,b,c)
