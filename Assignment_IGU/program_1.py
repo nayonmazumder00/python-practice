e550ec8031a968cc1d6b7a9c664fce25b14248b1
Nayon Mazumder <nayonmazumder00@gmail.com>
2022-09-18 15:31:16 +0530

    program 1 compeleted

---
+++ Assignment_IGU/program_1.py
@@ -0,0 +1,12 @@
+""" Name: NAYON MAZUMDER
+    REG : 210011015059".
+    Dept. Of CSE, IGU, INDIA
+"""
+
+a=int(input("Please Enter the value of A : "))
+b=int(input("Please Enter the value of B : "))
+c=int(input("Please Enter the value of C : "))
+
+d=a*b*c
+
+print("The multi of Variables : ", d)
