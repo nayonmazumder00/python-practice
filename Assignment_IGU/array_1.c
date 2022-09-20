e1fbc3ae0e6a155afc12459752943fba3546909c
Nayon Mazumder <nayonmazumder00@gmail.com>
2022-09-20 20:53:04 +0530

    program5&6

---
+++ Assignment_IGU/array_1.c
@@ -0,0 +1,18 @@
+// Name: NAYON MAZUMDER
+//  REG : 210011015059".
+//  Dept. Of CSE, IGU, INDIA
+
+#include<stdio.h>
+int main(){
+    int i, j, ara[12];
+    for(i=0;i<11;i++){
+        printf("Please Enter The value of ara[%d] : ",i);
+        scanf("%d",&ara[i]);
+    }
+    for(j=0;j<=10;j++){
+        printf("%d\t", ara[j]);
+    
+    }
+    return 0;
+    
+}
